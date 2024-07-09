c9810ebce473243f54f479e6fba97347053799bd
Axel Cocat <ax.cocat@gmail.com>
2024-07-09 16:01:04 +0200

    feat(charactersDbService): add getCharacter

--- server/services/db/charactersDbService.ts
+++ server/services/db/charactersDbService.ts
@@ -11,14 +11,37 @@ export class CharactersDbService extends DbServiceBase {
 		);
 
 		return (rows as any[]).map((row) => ({
-			guid: row.guid,
-			name: row.name,
-			race: row.race,
-			class: row.class,
-			gender: row.gender,
-			level: row.level,
+			guid: row.guid as number,
+			name: row.name as string,
+			race: row.race as number,
+			class: row.class as number,
+			gender: row.gender as number,
+			level: row.level as number,
 			online: row.online === 1,
 		}));
+	}
+
+	public async getCharacter(characterId: number) {
+		const [rows] = await this.db.query(
+			`SELECT account, name, race, class, gender, level, online
+			FROM characters
+			WHERE guid = ?`,
+			[characterId]
+		);
+		if ((rows as any[]).length === 0) {
+			return null;
+		}
+
+		const char = rows[0];
+		return {
+			account: char.account as number,
+			name: char.name as string,
+			race: char.race as number,
+			class: char.class as number,
+			gender: char.gender as number,
+			level: char.level as number,
+			online: char.online === 1,
+		};
 	}
 
 	public async getGuildsOwnedByAccount(accountId: number) {
